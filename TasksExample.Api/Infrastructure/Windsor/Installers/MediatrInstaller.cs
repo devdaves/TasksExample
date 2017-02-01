5340dd39c1df2051801ee14fb827ecaeeb9c78a3
Dave Stensgaard <dave.stensgaard@alaskaair.com>
2017-02-01 08:35:04 -0800

    fix startup bug with IOC container when requesting generic type for the first time

--- TasksExample.Api/Infrastructure/Windsor/Installers/MediatrInstaller.cs
+++ TasksExample.Api/Infrastructure/Windsor/Installers/MediatrInstaller.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Castle.MicroKernel;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
@@ -16,8 +17,20 @@ namespace TasksExample.Api.Infrastructure.Windsor.Installers
             container.Kernel.AddHandlersFilter(new ContravariantFilter());
             container.Register(Classes.FromThisAssembly().Pick().WithServiceAllInterfaces());
             container.Register(Component.For<IMediator>().ImplementedBy<Mediator>());
-            container.Register(Component.For<SingleInstanceFactory>().UsingFactoryMethod<SingleInstanceFactory>(k => t => k.Resolve(t)));
+            container.Register(Component.For<SingleInstanceFactory>().UsingFactoryMethod<SingleInstanceFactory>(k => t => Resolve(k, t)));
             container.Register(Component.For<MultiInstanceFactory>().UsingFactoryMethod<MultiInstanceFactory>(k => t => (IEnumerable<object>)k.ResolveAll(t)));
+        }
+
+        private static object Resolve(IKernel k, Type t)
+        {
+            try
+            {
+                return k.Resolve(t);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
